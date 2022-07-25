3e8de795763f3b9874c6374c902d0a2d920959a4
L3OL1N <z3650888@gmail.com>
2022-07-25 13:34:47 +0800

    Feature book read sumit valid

--- Library.js
+++ Library.js
@@ -16,17 +16,15 @@ function Book() {
     this.read;
 }
 // appear form
-function creatForm(){
-    let newForm = document.createElement("form");
-    let textNode = document.createTextNode("test");
-    newForm.appendChild(textNode);
-    newForm.classList.add("input");
-    wrapDiv.appendChild(newForm);
-}
 function showForm() {
     formDiv.style.display = "block";
 }
 function sumit(){
+    let text = form.elements.title.value;
+    if(text === "") {
+        alert("Book need title");
+        return false;
+    }
     let newBook = new Book;
     newBook.title = form.elements.title.value;
     newBook.author = form.elements.author.value;
@@ -38,8 +36,8 @@ function sumit(){
     formDiv.style.display = "none";
     console.dir(myLibrary);
     showLibrary();
-    
 }
+// dekete
 function deleteBookToLibrary(){
     let deleteTitle = prompt("search title");
     let found = myLibrary.find(Element => Element.title === deleteTitle);
@@ -50,7 +48,7 @@ function deleteBookToLibrary(){
     console.dir(myLibrary);
 }
 function deleteBook(e){
-    if(e.target.matches("button")){
+    if(e.target.matches("button[data-item]")){
         let found = myLibrary.find(Element => Element.title === e.target.dataset.item);
         if(found === undefined) return ;
         let index = myLibrary.indexOf(found);
@@ -59,31 +57,37 @@ function deleteBook(e){
     }
     showLibrary();
 }
+// is book read ?
+function readBook(e){
+    if(e.target.matches(".read")){
+        let found = myLibrary.find(Element => Element.title === e.target.dataset.title);
+        if(found === undefined) return ;
+        let index = myLibrary.indexOf(found);
+        myLibrary[index].read === "ture" ? 
+            Object.defineProperty(myLibrary[index],"read",{value:"false"}) : Object.defineProperty(myLibrary[index],"read",{value:"ture"});
+        console.log(myLibrary[index].read);
+        showLibrary();
+    }
+}
 //add book info
 function showLibrary(){
     let times = (libraryDiv.offsetWidth - 20) / 5.5 ;
+    let readStatus = "Unread";
     libraryDiv.innerHTML = "";
     for(let i = 0; i < myLibrary.length; i++){
+        if(myLibrary[i].read === "ture") readStatus = "Read";
+        if(myLibrary[i].read === "false") readStatus = "Unread";
         libraryDiv.innerHTML += `${myLibrary[i].title} --- > author : ${myLibrary[i].author} / pages : ${myLibrary[i].pages}
-        <button class="read">Read</button>
+        <button class="read" data-title= "${myLibrary[i].title}" data-read="${myLibrary[i].read}">${readStatus}</button>
         <button data-item=${myLibrary[i].title}>delete</button>
         <br>${"-".repeat(times)}<br>`;
     }
-}
-for(let i = 0; i < myLibrary.length; i++){
-    let newP = document.createElement("p");
-    let newBtn = document.createElement("button");
-    let TextNode = document.createTextNode("test");
-    newP.appendChild(textNode);
-    libraryDiv.appendChild(newP);
 }
 
 addBtn.addEventListener("click",showForm);
 deleteBtn.addEventListener("click",deleteBookToLibrary);
 
-//showBtn.addEventListener("click",showLibrary);
 sumitBtn.addEventListener("click",sumit);
-
-let deleteBtn2 = document.querySelectorAll("[data-item]");
-
+// book btn
 libraryDiv.addEventListener("click",deleteBook);
+libraryDiv.addEventListener("click",readBook);
